5c56f3d03924908473d2a0ad7b51fc67cda21706
Eva Howard <7960.eva@gmail.com>
2025-05-17 13:50:53 -0700

    made ShoppingList singleton

--- StartApp.py
+++ StartApp.py
@@ -3,9 +3,4 @@ import FridgeHome
 
 class StartGame:
     pygame.init()
-    SCREEN_WIDTH = 810
-    SCREEN_HEIGHT = 810
-
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    FridgeHome.run(screen)
+    FridgeHome.run()
