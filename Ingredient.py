ae66988e99632d85078d249bf78499f77c4f68dd
2frogfrog <2frogfrog@gmail.com>
2025-05-17 11:24:41 -0700

    Made Ingredient

---
+++ Ingredient.py
@@ -0,0 +1,6 @@
+class Ingredient:
+    def __init__(self,name,shelfLife,quantity,quantityUnit):
+        self.Name = name
+        self.shelfLife = shelfLife
+        self.quantity = quantity
+        self.quantityUnit = quantityUnit
