f20150c6988374de2e715fe93300d09eff6177b6
2frogfrog <2frogfrog@gmail.com>
2025-05-17 12:11:45 -0700

    Updated fridge method

--- FridgeHome.py
+++ FridgeHome.py
@@ -1,1 +1,13 @@
-def run()
+import Ingredient
+
+def run():
+    return 0
+
+class Fridge:
+    def __init__(self):
+        self.ingredient_list = []
+
+    def addIngredient(self, name, doesExpire, shelfLife, quantity, quantityUnit):
+        newIngredient = Ingredient(name,shelfLife,doesExpire,quantity,quantityUnit)
+        self.ingredient_list.append(newIngredient)
+
