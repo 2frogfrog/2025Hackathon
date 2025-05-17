2c79de9075b4082826be729635b8f335cb27772a
Eva Howard <7960.eva@gmail.com>
2025-05-17 11:37:43 -0700

    made shopping list class

---
+++ ShoppingList.py
@@ -0,0 +1,24 @@
+# shopping_list.py
+
+class ShoppingList:
+    def __init__(self):
+        self.items = []
+
+    def add_ingredient(self, ingredient):
+        """Add an ingredient object to the shopping list if its name is not already present."""
+        if not any(item.name == ingredient.name for item in self.items):
+            self.items.append(ingredient)
+        else:
+            print(f"Ingredient '{ingredient.name}' already in the shopping list.")
+
+    def delete_ingredient(self, ingredient):
+        """Delete an ingredient object from the shopping list by matching its name."""
+        for item in self.items:
+            if item.name == ingredient.name:
+                self.items.remove(item)
+                break
+        else:
+            print(f"Ingredient '{ingredient.name}' not found in the shopping list.")
+
+    def __str__(self):
+        return "Shopping List: [" + ", ".join(item.name for item in self.items) + "]"
