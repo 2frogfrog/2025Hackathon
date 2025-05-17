ffbb9a6f22a427df8571ed2a39b66f6144654e13
2frogfrog <2frogfrog@gmail.com>
2025-05-17 13:33:10 -0700

    priority ingredients and recipes

--- FridgeHome.py
+++ FridgeHome.py
@@ -1,11 +1,12 @@
-import Ingredient
+from Ingredient import Ingredient
 
 def run():
     return 0
 
 class Fridge:
-    ingredient_list = []
-    def addIngredient(self, name, doesExpire, shelfLife, quantity, quantityUnit):
+    def __init__(self):
+        self.ingredient_list = []
+    def add_ingredient(self, name, doesExpire, shelfLife, quantity, quantityUnit):
         new_ingredient = Ingredient(name,shelfLife,doesExpire,quantity,quantityUnit)
         self.ingredient_list.append(new_ingredient)
         self.ingredient_list = sorted(self.ingredient_list)
